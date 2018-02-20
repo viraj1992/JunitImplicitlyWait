36d03495c2bda4987e92ef5094f93c4b7bc2d1ba
Viraj Pai <coder.viraj@gmail.com>
2018-02-21 00:06:11 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+	
+	private WebDriver driver;
+	private String baseUrl;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Viraj\\Desktop\\Selenium Drivers\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		baseUrl = "https://letskodeit.teachable.com/p/practice";
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void test() {
+		driver.get(baseUrl);
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.id("user_email")).sendKeys("admin@mailinator.com");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		Thread.sleep(2000);
+		driver.quit();
+	}
+
+	
+
+}
